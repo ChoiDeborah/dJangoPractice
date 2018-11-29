ecff62530aae2058ae87603966f66bc388158a57
ChoiDeobrah <adorablejimini@gmail.com>
2018-11-29 17:17:30 +0900

    fix mysite/urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import include, url
-from . import views
-
+from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
-    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
-    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('blog.urls')),
 ]
