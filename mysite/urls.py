af4d3168a8da483102ada96796485ffc5d1d631b
ChoiDeobrah <adorablejimini@gmail.com>
2018-11-30 12:00:28 +0900

    git Fix import django.contrib.auth.views Login Error

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,10 +17,11 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 from django.contrib.auth import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/login/$', views.login, name='login'),
-    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
+    url(r'^accounts/login/$', views.LoginView, name='login'),
+    url(r'^accounts/logout/$', views.LogoutView, name='logout', kwargs={'next_page': '/'}),
     url(r'', include('blog.urls')),
 ]
